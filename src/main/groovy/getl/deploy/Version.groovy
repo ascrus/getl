2de0e6338122446bd41587855a36a14bba3d00f7
ascrus <ascrus@2e8a96de-90c8-44f1-a72a-b77d5aaa7902>
2015-10-20 10:09:03 +0000

    git-svn-id: http://svn.code.sf.net/p/getl/source/branches/1.1.40/getl@661 2e8a96de-90c8-44f1-a72a-b77d5aaa7902

--- src/main/groovy/getl/deploy/Version.groovy
+++ src/main/groovy/getl/deploy/Version.groovy
@@ -8,12 +8,12 @@ class Version {
 	/**
 	 * GETL version
 	 */
-	public static version = "1.1.39"
+	public static version = "1.1.40"
 	
 	/**
 	 * GETL version as numeric
 	 */
-	public static versionNum = 1.0139
+	public static versionNum = 1.0140
 
 	/**
 	 * Compatibility GETL version
