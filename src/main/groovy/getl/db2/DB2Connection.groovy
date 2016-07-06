60fe36c0fabb20808c39d79c6c77054bb03f2070
Alexsey Konstantinov <ascrus@easydata.ru>
2016-07-07 00:35:57 +0300

    getl.db2: added support IBM DB2 driver.

---
+++ src/main/groovy/getl/db2/DB2Connection.groovy
@@ -0,0 +1,57 @@
+/*
+ GETL - based package in Groovy, which automates the work of loading and transforming data. His name is an acronym for "Groovy ETL".
+
+ GETL is a set of libraries of pre-built classes and objects that can be used to solve problems unpacking,
+ transform and load data into programs written in Groovy, or Java, as well as from any software that supports
+ the work with Java classes.
+ 
+ Copyright (C) 2013-2016  Alexsey Konstantonov (ASCRUS)
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License and
+ GNU Lesser General Public License along with this program.
+ If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package getl.db2
+
+import groovy.transform.InheritConstructors
+import getl.jdbc.JDBCConnection
+
+/**
+ * IBM DB2 connection class
+ * @author Alexsey Konstantinov
+ *
+ */
+@InheritConstructors
+class DB2Connection extends JDBCConnection {
+	DB2Connection() {
+		super(driver: DB2Driver)
+	}
+	
+	DB2Connection(Map params) {
+		super(new HashMap([driver: DB2Driver]) + params)
+		if (this.getClass().name == 'getl.db2.DB2Connection') methodParams.validation("Super", params)
+	}
+	
+	@Override
+	protected void onLoadConfig (Map configSection) {
+		super.onLoadConfig(configSection)
+		if (this.getClass().name == 'getl.db2.DB2Connection') methodParams.validation("Super", params)
+	}
+	
+	@Override
+	protected void doInitConnection () {
+		super.doInitConnection()
+		driverName = "com.ibm.db2.jcc.DB2Driver"
+	}
+}
---
+++ src/main/groovy/getl/db2/DB2Driver.groovy
@@ -0,0 +1,65 @@
+/*
+ GETL - based package in Groovy, which automates the work of loading and transforming data. His name is an acronym for "Groovy ETL".
+
+ GETL is a set of libraries of pre-built classes and objects that can be used to solve problems unpacking,
+ transform and load data into programs written in Groovy, or Java, as well as from any software that supports
+ the work with Java classes.
+ 
+ Copyright (C) 2013-2016  Alexsey Konstantonov (ASCRUS)
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License and
+ GNU Lesser General Public License along with this program.
+ If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package getl.db2
+
+import java.sql.PreparedStatement
+import getl.data.Dataset
+import getl.driver.Driver
+import getl.jdbc.JDBCDriver
+import groovy.transform.InheritConstructors
+
+
+/**
+ * MSSQL driver class
+ * @author Alexsey Konstantinov
+ *
+ */
+@InheritConstructors
+class DB2Driver extends JDBCDriver {
+	DB2Driver () {
+		super()
+		
+		caseObjectName = "UPPER"
+	}
+	
+	@Override
+	public List<Driver.Operation> operations() {
+		List<Driver.Operation> result = super.operations()
+		result << Driver.Operation.CREATE
+		result
+	}
+	
+	@Override
+	public List<Driver.Support> supported() {
+		List<Driver.Support> result = super.supported()
+		result << Driver.Support.INDEX
+		result
+	}
+	
+	@Override
+	public String defaultConnectURL () {
+		"jdbc:db2://{host}/{database}"
+	}
+}
