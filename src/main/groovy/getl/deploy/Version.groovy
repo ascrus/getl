6c0c9996a3d228de919b329e657923f3dc9d8af7
Alexsey Konstantinov <ascrus@easydata.ru>
2017-02-28 18:39:26 +0300

    gradle: change version to 1.2.12 getl.vertica.ReverseEngineering: added support udf sql functions

--- src/main/groovy/getl/deploy/Version.groovy
+++ src/main/groovy/getl/deploy/Version.groovy
@@ -32,12 +32,12 @@ class Version {
 	/**
 	 * GETL version
 	 */
-	public static version = "1.2.11"
+	public static version = "1.2.12"
 	
 	/**
 	 * GETL version as numeric
 	 */
-	public static versionNum = 1.0211
+	public static versionNum = 1.0212
 
 	/**
 	 * Compatibility GETL version
