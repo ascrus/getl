9fffc5e85f61d947d86721e381cef2173c505992
Алексей Константинов <ascrus@yandex.ru>
2018-02-01 16:02:17 +0300

    Change version to 1.3.9

--- src/main/groovy/getl/deploy/Version.groovy
+++ src/main/groovy/getl/deploy/Version.groovy
@@ -32,12 +32,12 @@ class Version {
 	/**
 	 * GETL version
 	 */
-	public static version = "1.3.8"
+	public static version = "1.3.9"
 	
 	/**
 	 * GETL version as numeric
 	 */
-	public static versionNum = 1.0308
+	public static versionNum = 1.0309
 
 	/**
 	 * Compatibility GETL version
